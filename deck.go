463f3dcdbba989a3145a2cb16a19fa2fe55dcff9
glover <info.copdev@gmail.com>
2024-10-30 08:24:28 +0100

    feat(app): save to file functionality

--- deck.go
+++ deck.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"strings"
+)
 
 type deck []string
 
@@ -24,4 +28,17 @@ func (d deck) print() {
 	for i, card := range d {
 		fmt.Println(i, card)
 	}
+}
+
+func deal (d deck, handsize int) (deck, deck) {
+  return d[:handsize], d[handsize:]
+}
+
+
+func (d deck) toString() string {
+  return strings.Join([]string(d), ",")
+}
+
+func (d deck) saveToFile(filename string) error {
+  return ioutil.WriteFile(filename, []byte(d.toString()), 0666)
 }
